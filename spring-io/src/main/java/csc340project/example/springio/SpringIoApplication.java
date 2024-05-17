22d05acf0af773cd8ffb09a3ea1d9def3b96f783
cism829 <clrodgers@uncg.edu>
2024-05-16 20:34:05 -0400

    LFG-8

---
+++ spring-io/src/main/java/csc340project/example/springio/SpringIoApplication.java
@@ -0,0 +1,13 @@
+package csc340project.example.springio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIoApplication.class, args);
+	}
+
+}
---
+++ spring-io/src/test/java/csc340project/example/springio/SpringIoApplicationTests.java
@@ -0,0 +1,13 @@
+package csc340project.example.springio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
