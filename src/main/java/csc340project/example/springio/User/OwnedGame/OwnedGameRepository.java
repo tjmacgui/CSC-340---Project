3d9381353e0e85efb3fbfe6f5f49d6d154d9bc88
thomasma <tjmacgui@uncg.edu>
2024-06-09 21:48:45 -0400

    SteamAPI working

--- src/main/java/csc340project/example/springio/User/OwnedGame/OwnedGameRepository.java
+++ src/main/java/csc340project/example/springio/User/OwnedGame/OwnedGameRepository.java
@@ -1,10 +1,13 @@
 package csc340project.example.springio.User.OwnedGame;
 
+import csc340project.example.springio.GameListings.Listing;
 import csc340project.example.springio.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OwnedGameRepository extends JpaRepository<OwnedGame, Integer> {
     List<OwnedGame> findByUser(User user);
+    Optional<OwnedGame> findByUserAndListing(User user, Listing listing);
 }
